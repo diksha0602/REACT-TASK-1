a7f36dac030873c15529c6b7ec781b9f5b8f557c
Your Name <you@example.com>
2023-11-11 00:09:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css"; // Import your global CSS if needed
+import ParagraphWordCounter from "./ParagraphWordCounter";
+
+function App() {
+  return (
+    <div className="App">
+      <ParagraphWordCounter />
+    </div>
+  );
+}
+
+export default App;
